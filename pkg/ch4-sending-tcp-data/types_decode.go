f4941f559178929241cbf0b958e843a38bd729dc
kochetov_ds <kochetov_ds@automiq.ru>
2024-12-01 18:43:48 +0600

    Add encode function, change decode signature
    Leave all the marshalling logic in two counterpart functions - encode
    and decode.
    Takeaways:
    - how to switch-case on interface underlying types;
    - how to pass an interface instance to a function and how interfaces
      store their implementations (either by value or by pointer)

--- pkg/ch4-sending-tcp-data/types_decode.go
+++ pkg/ch4-sending-tcp-data/types_decode.go
@@ -3,16 +3,52 @@ package ch4
 import (
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"io"
 )
 
-func decode(r io.Reader) (Payload, error) {
+func encode(w io.Writer, p Payload) (int64, error) {
+    var typ uint8
+    var err error
+    var n int64
+
+    switch p.(type) {
+    case *Binary:
+        typ = BinaryType
+    case *String:
+        typ = StringType
+    default:
+        err = errors.New("invalid payload type")
+    }
+
+    if err != nil {
+        return n, err
+    }
+
+    err = binary.Write(w, binary.BigEndian, typ)
+    if err != nil {
+        return n, err
+    }
+    n += 1
+
+    o, err := p.WriteTo(w)
+    if err != nil {
+        return n, err
+    }
+    n += o
+
+    return n, nil
+}
+
+func decode(r io.Reader, p *Payload) (int64, error) {
+    var n int64 = 0
     var typ uint8
 
     err := binary.Read(r, binary.BigEndian, &typ)
     if err != nil {
-        return nil, err
+        return n, err
     }
+    n += 1
 
     var payload Payload
     switch typ {
@@ -21,18 +57,22 @@ func decode(r io.Reader) (Payload, error) {
     case BinaryType:
         payload = NewBinary()
     default:
-        err = errors.New("unknown type")
+        msg := fmt.Sprintf("invalid type: %d", typ)
+        err = errors.New(msg)
     }
 
     if err != nil {
-        return nil, err
+        return n, err
     }
 
-    _, err = payload.ReadFrom(r)
+    o, err := payload.ReadFrom(r)
     if err != nil {
-        return nil, err
+        return n, err
     }
+    n += o
 
-    return payload, nil
+    *p = payload
+
+    return n, nil
 }
 
