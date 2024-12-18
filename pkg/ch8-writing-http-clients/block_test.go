e5567217896e75d7c5e61525808a53e01992df23
Dima Kochetov <dima.kochetov.70@gmail.com>
2024-12-18 10:09:23 +0500

    Extend block test
    The second test case shows how to do HTTP requests with timeouts to
    avoid endless blocking while waiting for a response

--- pkg/ch8-writing-http-clients/block_test.go
+++ pkg/ch8-writing-http-clients/block_test.go
@@ -1,9 +1,12 @@
 package ch8
 
 import (
+	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func blockIndefinitely(_ http.ResponseWriter, _ *http.Request) {
@@ -11,7 +14,38 @@ func blockIndefinitely(_ http.ResponseWriter, _ *http.Request) {
 }
 
 func TestBlockIndefinitely(t *testing.T) {
+	// Would ruin the whole test if uncommented
+	// ts := httptest.NewServer(http.HandlerFunc(blockIndefinitely))
+	// _, _ = http.Get(ts.URL)
+	// t.Fatal("client did not indefinitely block")
+}
+
+func TestBlockIndefinitelyWithTimeout(t *testing.T) {
+	// Create a new server with an infinitely blocking handler
 	ts := httptest.NewServer(http.HandlerFunc(blockIndefinitely))
-	_, _ = http.Get(ts.URL)
-	t.Fatal("client did not indefinitely block")
+
+	// Create a context with timeout of 5 second
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// Cancel at scope exit
+	defer func() {
+		cancel()
+	}()
+
+	// Create a new HTTP GET request specifying the context with timeout, the method, and the server's URL
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, ts.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Execute the request using the default client
+	resp, err := http.DefaultClient.Do(req)
+	// Check that the error is an exceeded deadline
+	if err != nil {
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Fatal(err)
+		}
+		return
+	}
+	// Close the body
+	_ = resp.Body.Close()
 }
