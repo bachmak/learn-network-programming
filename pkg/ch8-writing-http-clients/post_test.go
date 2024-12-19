480be87d6a26592eaa127b09a648982f35dfefca
Dima Kochetov <dima.kochetov.70@gmail.com>
2024-12-20 01:19:03 +0500

    Add multipart post request test
    The test shows how to make complex POST request with content consisting
    of parts of different types using multiwriter.
    Takeaways:
    - deferred functions are called on function exit, not scope;
    - how to build complex request content and create requests with
      contexts

--- pkg/ch8-writing-http-clients/post_test.go
+++ pkg/ch8-writing-http-clients/post_test.go
@@ -2,12 +2,17 @@ package ch8
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
+	"time"
 )
 
 type User struct {
@@ -87,4 +92,100 @@ func TestPostUser(t *testing.T) {
 			http.StatusAccepted, resp.StatusCode,
 		)
 	}
+}
+
+// Func TestMultipartPost
+func TestMultipartPost(t *testing.T) {
+	// Creat a new buffer for filling with request data
+	buf := new(bytes.Buffer)
+	// Create a multiwriter operating on the buffer
+	mw := multipart.NewWriter(buf)
+
+	// Iterate through a map with key-values for form fields
+	for k, v := range map[string]string{
+		"date":        time.Now().Format(time.RFC1123),
+		"description": "this is just my description",
+	} {
+		// Simply write each field to the multiwriter
+		err := mw.WriteField(k, v)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// Then the two files
+	for i, file := range []string{
+		"./files/hello.txt",
+		"./files/goodbye.txt",
+	} {
+		// First, create a file part, given a field and a file name
+		fieldname := fmt.Sprintf("file%d", i)
+		filename := filepath.Base(file)
+		filePart, err := mw.CreateFormFile(fieldname, filename)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Open a file
+		f, err := os.Open(file)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Copy the file's content to the filePart object
+		_, err = io.Copy(filePart, f)
+		// Close the file
+		_ = f.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// Close the writer, the request should be ready at this point
+	err := mw.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a context with timeout (long enough, since we'll do a real request)
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	// Cancel at the scope exit
+	defer func() {
+		cancel()
+	}()
+
+	// Create a POST request object to httpbin.org/post, with the context
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, "https://httpbin.org/post", buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Set content type from the multiwriter
+	req.Header.Set("Content-Type", mw.FormDataContentType())
+
+	// Make a request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Drain the body at the scope exit
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	// Read the full response to a byte-slice
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Check that the status code is OK
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf(
+			"expected status %d; actual status %d",
+			http.StatusOK, resp.StatusCode,
+		)
+	}
+
+	// Log the response body
+	t.Logf("\n%s", b)
 }
