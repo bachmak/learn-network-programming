93b96e7303ec6007247ae1d541c48535905dcf37
Dima Kochetov <dima.kochetov.70@gmail.com>
2024-12-26 20:07:04 +0500

    Add custom Caddy build
    The built configuration includes previously implemented middleware and
    TOML config adapter

---
+++ pkg/ch10-caddy/caddy/caddy.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	cmd "github.com/caddyserver/caddy/v2/cmd"
+	_ "github.com/caddyserver/caddy/v2/modules/standard"
+
+	_ "learn-network-programming/pkg/ch10-caddy/restrict_prefix"
+	_ "learn-network-programming/pkg/ch10-caddy/toml_adapter"
+)
+
+func main() {
+	cmd.Main()
+}
