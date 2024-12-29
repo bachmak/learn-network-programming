02d4a34a7021c0c900d09193ff32fc0feceedf40
Dima Kochetov <dima.kochetov.70@gmail.com>
2024-12-29 23:12:16 +0500

    Add example zap JSON test
    The test shows how to create a logger with custom configuration which
    includes:
    - encoder that uses a specific format (JSON) and a custom config;
    - write syncer, which defines where the logs are sunk + adds
      synchronization;
    - core, which defines the encoder, the syncer, and the log-level
      threshold;
    - additional options, like: whether to include the caller, the
      stack-trace, add hooks, include additional fields
    
    Takeaways:
    - Zap's configuration facilities are very flexible and highly
      customizable;
    - zap.Logger allows creating some kind of a nested or child logger,
      which seems to play well in cases where log hierarchy is needed

--- pkg/ch13-logging-and-metrics/zap_test.go
+++ pkg/ch13-logging-and-metrics/zap_test.go
@@ -1,6 +1,11 @@
 package ch13
 
-import "go.uber.org/zap/zapcore"
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
 // encoder config (agnostic of specific formats encoders use)
 var encoderConfig = zapcore.EncoderConfig{
@@ -10,7 +15,7 @@ var encoderConfig = zapcore.EncoderConfig{
 
 	// level keys + level format
 	LevelKey:    "level",
-	EncodeLevel: zapcore.LowercaseColorLevelEncoder,
+	EncodeLevel: zapcore.LowercaseLevelEncoder,
 
 	// caller key + caller format
 	CallerKey:    "caller",
@@ -19,4 +24,40 @@ var encoderConfig = zapcore.EncoderConfig{
 	// time key + time format (not used for deterministic tests)
 	// TimeKey: "time",
 	// EncodeTime: zapcore.ISO8601TimeEncoder,
+}
+
+// func Example_zapJSON
+func Example_zapJSON() {
+	// new JSON encoder from the config
+	encoder := zapcore.NewJSONEncoder(encoderConfig)
+	// write syncer to stduot
+	syncer := zapcore.Lock(os.Stdout)
+	// new core, log level = debug
+	core := zapcore.NewCore(encoder, syncer, zapcore.DebugLevel)
+
+	// options:
+	options := []zap.Option{
+		// log the caller
+		zap.AddCaller(),
+		// add fields: go runtime version (stubbed for future test correctness)
+		zap.Fields(zap.String("version", "go1.23.4")),
+	}
+
+	// new logger from the core and the options
+	zl := zap.New(core, options...)
+	// flush at scope exit
+	defer func() {
+		_ = zl.Sync()
+	}()
+
+	// create a "child" logger, named "example"
+	example := zl.Named("example")
+	// log debug message
+	example.Debug("test debug message")
+	// log info message
+	example.Info("test info message")
+
+	// Output:
+	// {"level":"debug","name":"example","caller":"ch13-logging-and-metrics/zap_test.go:56","msg":"test debug message","version":"go1.23.4"}
+	// {"level":"info","name":"example","caller":"ch13-logging-and-metrics/zap_test.go:58","msg":"test info message","version":"go1.23.4"}
 }
