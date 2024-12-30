a70ec5a18661125c317e3bb30667509a59bb0e13
Dima Kochetov <dima.kochetov.70@gmail.com>
2024-12-30 13:55:31 +0500

    Add zap InfoFile DebugConsole test
    The test shows how to extend an existing logger in runtime to add an
    additional sink in a different format with modified options and keep
    using it (or use it as a new separate logger).
    Takeaways:
    - zap is even more extensible and configurable than I initially thought

--- pkg/ch13-logging-and-metrics/zap_test.go
+++ pkg/ch13-logging-and-metrics/zap_test.go
@@ -1,6 +1,8 @@
 package ch13
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 
 	"go.uber.org/zap"
@@ -58,8 +60,8 @@ func Example_zapJSON() {
 	example.Info("test info message")
 
 	// Output:
-	// {"level":"debug","name":"example","caller":"ch13-logging-and-metrics/zap_test.go:56","msg":"test debug message","version":"go1.23.4"}
-	// {"level":"info","name":"example","caller":"ch13-logging-and-metrics/zap_test.go:58","msg":"test info message","version":"go1.23.4"}
+	// {"level":"debug","name":"example","caller":"ch13-logging-and-metrics/zap_test.go:58","msg":"test debug message","version":"go1.23.4"}
+	// {"level":"info","name":"example","caller":"ch13-logging-and-metrics/zap_test.go:60","msg":"test info message","version":"go1.23.4"}
 }
 
 func Example_zapConsole() {
@@ -85,4 +87,74 @@ func Example_zapConsole() {
 	// Output:
 	// info	[console]	this is logged by the logger
 	// error	[console]	this is also logged by the logger
+}
+
+// func Example_zapInfoFileDebugConsole
+func Example_zapInfoFileDebugConsole() {
+	// mock a log file
+	logFile := new(bytes.Buffer)
+
+	// default core: json encoder, sync to file, level = info
+	encoder := zapcore.NewJSONEncoder(encoderConfig)
+	syncer := zapcore.AddSync(logFile)
+	core := zapcore.NewCore(encoder, syncer, zapcore.InfoLevel)
+
+	// deafult loger from the default core
+	zl := zap.New(core)
+	defer func() {
+		_ = zl.Sync()
+	}()
+
+	// log some initial messages: debug (shouldn't be displayed) + error
+	zl.Debug("this is below the logger's threshold and won't log")
+	zl.Error("this is logged by the logger")
+
+	// core wrapper, to "branch" additional logger
+	coreWrapper := func(core zapcore.Core) zapcore.Core {
+		// copy encoder config to modify a copy
+		configCopy := encoderConfig
+		// capitalize log level as an example modification
+		configCopy.EncodeLevel = zapcore.CapitalLevelEncoder
+
+		// new console encoder, console syncer, debug-level console
+		encoder := zapcore.NewConsoleEncoder(configCopy)
+		syncer := zapcore.Lock(os.Stdout)
+		consoleCore := zapcore.NewCore(encoder, syncer, zapcore.DebugLevel)
+
+		// merge wrapped core and new core
+		cores := []zapcore.Core{
+			core,
+			consoleCore,
+		}
+
+		// create tee to log to the two cores at the same time
+		return zapcore.NewTee(cores...)
+	}
+
+	// specify options for the new logger
+	options := []zap.Option{
+		zap.WrapCore(coreWrapper),
+	}
+
+	// modify logger with options
+	// (at this point we start logging to both console and file)
+	zl = zl.WithOptions(options...)
+
+	// log header
+	fmt.Println("standard output:")
+	// log debug + info
+	zl.Debug("this is only logged as console encoding")
+	zl.Info("this is logged as console encoding and JSON")
+
+	// print log file contents
+	fmt.Print("\nfile contents:\n", logFile.String())
+
+	// Output:
+	// standard output:
+	// DEBUG	this is only logged as console encoding
+	// INFO	this is logged as console encoding and JSON
+	//
+	// file contents:
+	// {"level":"error","msg":"this is logged by the logger"}
+	// {"level":"info","msg":"this is logged as console encoding and JSON"}
 }
